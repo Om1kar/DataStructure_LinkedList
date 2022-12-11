5735bc42fdb5980ff5984e51017dc706ca4fd71b
omkar <omkardudhewar121@gmail.com>
2022-12-11 15:01:10 +0530

    added: welcome message

---
+++ LinkedListMain.java
@@ -0,0 +1,7 @@
+package LinkedList;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("-----Welcome to Data Structure Linked List-----");
+    }
+}
